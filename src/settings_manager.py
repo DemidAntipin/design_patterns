1b4ec19c0c6509d0ec1df65e677d3fa280c96521
DemidAntipin <demidantipin@inbox.ru>
2025-09-21 23:14:44 +0800

    Added Settings class, updated settings_manager, updated tests

--- src/settings_manager.py
+++ src/settings_manager.py
@@ -1,59 +1,73 @@
-from src.models.company_model import company_model
+from src.models.settings import Settings
 import json
 import os
 
 ####################################################3
 # Менеджер настроек. 
 # Предназначен для управления настройками и хранения параметров приложения
-class settings_manager():
-    __file_name:str = ""
-    __company:company_model = None
+class Settings_manager():
+    __config_file:str = ""
+    __settings:Settings = None
+
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Settings_manager, cls).__new__(cls)
+        return cls.instance
 
     def __init__(self):
         self.default()
 
-    # Singletone
-    def __new__(cls):
-        if not hasattr(cls, 'instance'):
-            cls.instance = super(settings_manager, cls).__new__(cls)
-        return cls.instance
-
-    # Параметры организации из настроек
     @property
-    def company(self) -> company_model:
-        return self.__company
+    def settings(self) -> Settings:
+        return self.__settings
+    
+    @settings.setter
+    def settings(self, value: Settings):
+        if isinstance(value, Settings):
+            self.__settings = value
     
     @property
-    def file_name(self) -> str:
-        return self.__file_name
+    def config_file(self) -> str:
+        return self.__config_file
 
-    # Полный путь к файлу настроек
-    @file_name.setter
-    def file_name(self, value:str):
+    @config_file.setter
+    def config_file(self, value:str):
         if value.strip() == "":
             return
         if os.path.exists(value):
-            self.__file_name = value.strip()
+            self.__config_file = value.strip()
         else:
-            raise FileNotFoundError("Не найден файл настроек!")
+            raise FileNotFoundError("Не найден config файл!")
 
-    # Загрузить настройки из Json файла
-    def load(self):
-        if self.__file_name.strip() == "":
-            raise FileNotFoundError("Не найден файл настроек!")
+    def load(self) -> bool:
         try:
-            with open( self.__file_name.strip(), 'r') as file_instance:
-                data= json.load(file_instance)
-                if "company" in data.keys():
-                    item = data["company"]
-                    if "name" in item.keys():
-                        self.__company.name = item["name"]
-                        return True
-            return False
+            with open( self.__config_file.strip(), 'r') as file_instance:
+                return self.convert(json.load(file_instance))
         except:
             return False
+        
+    def convert(self, data: dict) -> bool:
+        if "company" in data.keys():
+            item = data["company"]
+            required_keys = ["name", "inn", "account", "correspondent_account", "bic", "ownership"]
+            missing_keys = [key for key in required_keys if key not in item]
+            if missing_keys:
+                raise KeyError(f"Отсутствуют обязательные поля: {', '.join(missing_keys)}!")
+            self.settings.company.name = item["name"]
+            self.settings.company.inn = item["inn"]
+            self.settings.company.account = item["account"]
+            self.settings.company.correspondent_account = item["correspondent_account"]
+            self.settings.company.bic = item["bic"]
+            self.settings.company.ownership = item["ownership"]
+            return True
+        else:
+            raise ValueError("Неверный формат config файла или файл поврежден!")
 
-    # Параметры настроек по умолчанию
     def default(self):
-        self.__company = company_model()
-        self.__company.name = "ООО Ромашка"
+        self.settings = Settings()
+        self.settings.company.name = "Default_name"
+        self.settings.company.inn = "000000000000"
+        self.settings.company.account = "00000000000"
+        self.settings.company.corr_account = "00000000000"
+        self.settings.company.bik = "000000000"
+        self.settings.company.ownership = ""
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,58 +1,177 @@
-from src.models.company_model import company_model
-from src.settings_manager import settings_manager
+from src.models.company_model import Company_model
+from src.settings_manager import Settings_manager
 import unittest
 
 class test_models(unittest.TestCase):
     # проверить создание основной модели
     # данные должны быть пустые
-    def test_empty_createmodel_company_model(self):
+    def test_create_empty_company(self):
         # подготовка
-        model = company_model()
+        model = Company_model()
         # действие
 
         # проверки
         assert model.name == ""
+        assert model.inn == ""
+        assert model.account == ""
+        assert model.correspondent_account == ""
+        assert model.bic == ""
+        assert model.ownership == ""
+
+    # проверить создание основной модели
+    # данные неверного типа
+    def test_invalid_types_company_model(self):
+        # подготовка
+        model = Company_model()
+
+        # действие
+
+        # проверки
+        with self.assertRaises(TypeError):
+            model.name = dict()
+        with self.assertRaises(TypeError):
+            model.inn = dict()
+        with self.assertRaises(TypeError):
+            model.account = dict()
+        with self.assertRaises(TypeError):
+            model.correspondent_account = dict()
+        with self.assertRaises(TypeError):
+            model.bic = dict()
+        with self.assertRaises(TypeError):
+            model.ownership = dict()
+
+    # проверить создание основной модели
+    # данные содержат ошибки 
+    def test_space_createmodel_company_model(self):
+        # подготовка
+        model = Company_model()
+        
+        # действие
+
+        # проверки
+        with self.assertRaises(ValueError):
+            model.name = "  "
+        with self.assertRaises(ValueError):
+            model.inn = 123
+        with self.assertRaises(ValueError):
+            model.account = 123
+        with self.assertRaises(ValueError):
+            model.correspondent_account = 123
+        with self.assertRaises(ValueError):
+            model.bic = 123
+        with self.assertRaises(ValueError):
+            model.ownership = "OOOAAAOOO"
+        
+
 
     # проверить создание основной модели
     # данные должны быть не пустые
     def test_notEmpty_createmodel_company_model(self):
         # подготовка
-        model = company_model()
+        model = Company_model()
 
         # действие
         model.name = "test"
+        model.inn = "123456789012"
+        model.account = "12345678901"
+        model.correspondent_account = "12345678901"
+        model.bic = "123456789"
+        model.ownership = "OOO"
 
         # проверки
         assert model.name == "test"
+        assert model.inn == "123456789012"
+        assert model.account == "12345678901"
+        assert model.correspondent_account == "12345678901"
+        assert model.bic == "123456789"
+        assert model.ownership == "OOO"
 
     # Проверить создание основной модели
     # Данные загружаются через json настройки
     def test_load_createmodel_company_model(self):
         # подготовка
         file_name = './settings.json'
-        manager = settings_manager()
-        manager.file_name=file_name
+        manager = Settings_manager()
+        manager.config_file=file_name
 
         # действие
         result = manager.load()
         
         # проверки
         assert result == True
+        assert manager.settings.company.name == "test"
+        assert manager.settings.company.inn == "123456789012"
+        assert manager.settings.company.account == "12345678901"
+        assert manager.settings.company.correspondent_account == "12345678901"
+        assert manager.settings.company.bic == "123456789"
+        assert manager.settings.company.ownership == "OOO"
+
+    # Проверить создание основной модели
+    # Данные загружаются через json настройки из другой директории
+    def test_load_other_directory(self):
+        # подготовка
+        file_name = './tests/data/other_settings.json'
+        manager = Settings_manager()
+        manager.config_file=file_name
+
+        # действие
+        result = manager.load()
+        
+        # проверки
+        assert result == True
+        assert manager.settings.company.name == "test"
+        assert manager.settings.company.inn == "123456789012"
+        assert manager.settings.company.account == "12345678901"
+        assert manager.settings.company.correspondent_account == "12345678901"
+        assert manager.settings.company.bic == "123456789"
+        assert manager.settings.company.ownership == "OOO"
+
+    # Проверить создание основной модели
+    # Файл настроек не существует
+    def test_load_not_exist_settings(self):
+        # подготовка
+        file_name = './invalid_settings.json'
+        manager = Settings_manager()
+
+        # действие
+        
+        # проверки
+        with self.assertRaises(FileNotFoundError):
+            manager.config_file=file_name
+
+    # Проверить создание основной модели
+    # Файл настроек поврежден
+    def test_load_bad_settings(self):
+        # подготовка
+        file_name = './tests/data/bad_settings.json'
+        manager = Settings_manager()
+        manager.config_file=file_name
+        file_name2 = './tests/data/bad_settings2.json'
+        manager2 = Settings_manager()
+        manager2.config_file=file_name2
+
+        # действие
+        result = manager.load()
+        result2 = manager2.load()
+        
+        # проверки
+        assert result == False
+        assert result2 == False
 
     # Проверить создание основной модели
     # Данные загружаем. Проверяем работу Singletone
     def test_loadCombo_createmodel_companymodel(self):
         # подготовка
-        file_name = './settings.json'
-        manager1 = settings_manager()
-        manager1.file_name = file_name
-        manager2 = settings_manager()
+        manager1 = Settings_manager()
+        manager2 = Settings_manager()
 
         # действие
-        manager1.load()
 
         # проверки
-        assert manager1.company == manager2.company
+        assert manager1.settings == manager2.settings
+        assert manager1.settings.company == manager2.settings.company
+
+    
 
 
 if __name__ == "__main__":
